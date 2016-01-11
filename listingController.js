57d4f3b418da2bdd4ad39c81912ccb5af74b1e7e
Annie Luc <annieluc@ufl.edu>
2016-01-11 12:26:19 -0500

    Implement methods to fulfill tasks of Assignment2

--- listingController.js
+++ listingController.js
@@ -3,12 +3,31 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    
+    //Adds a new listing to the listings variable and resets the information on the form
+    $scope.addListing = function() {
+      $scope.listings.push({
+        code: $scope.newCode,
+        name: $scope.newName,
+        coordinates: $scope.newCoordinates,
+        address: $scope.newAddress
+      });
+
+      //Resets the form 
+      $scope.newCode = '';
+      $scope.newName = '';
+      $scope.newCoordinates = '';
+      $scope.newAddress = '';
+    };
+
+    //Deletes a listing at the given index
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+
+    //Updates the detailed info to be shown on the Detailed Info jumbotron
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
