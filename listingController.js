59a0b230fc30e3c75156f0aea65745b6e5f97ede
Annie Luc <annieluc@ufl.edu>
2016-01-14 15:13:07 -0500

    Third round of CSS and map element

--- listingController.js
+++ listingController.js
@@ -1,7 +1,12 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
+
+    L.mapbox.accessToken = 'pk.eyJ1IjoiamF5Ymx1IiwiYSI6Inp1Uzg1WncifQ.ynx52ZMuvJFiRCmsx5JRlQ';
+    var map = L.mapbox.map('map', 'jayblu.jckk452m').setView([29.6520,-82.3250],14);
+
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.codeName = '';
 
     
     //Adds a new listing to the listings variable and resets the information on the form
@@ -28,7 +33,36 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
 
     //Updates the detailed info to be shown on the Detailed Info jumbotron
     $scope.showDetails = function(index) {
-      $scope.detailedInfo = $scope.listings[index];
+      if(typeof($scope.listings[index].coordinates) !== 'undefined'){
+        L.mapbox.featureLayer().clearLayers();
+        L.mapbox.featureLayer({
+          type: 'Feature',
+          geometry:{
+            type: 'Point',
+            coordinates:[
+              $scope.listings[index].coordinates.longitude,
+              $scope.listings[index].coordinates.latitude
+            ]
+          },
+          properties:{
+            title: $scope.listings[index].code + "\n" + $scope.listings[index].name,
+            description: $scope.listings[index].address,
+            'marker-size': 'large',
+            'marker-color': '#7E3661',
+            'marker-symbol': 'college'
+          }
+        }).addTo(map);
+        $scope.detailedInfo = $scope.listings[index].code+"\n\n"+
+                          $scope.listings[index].address+"\n\n("+
+                          $scope.listings[index].coordinates.latitude+", "+
+                          $scope.listings[index].coordinates.longitude+")\n\n";
+        $scope.codeName = $scope.listings[index].name;
+        map.setView([$scope.listings[index].coordinates.latitude, $scope.listings[index].coordinates.longitude], 16);
+      }else{
+        $scope.detailedInfo = "No Details";
+      }
+
+      //$scope.detailedInfo = $scope.listings[index];
     };
   }
 ]);
